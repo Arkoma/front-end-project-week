6d730074fd00bff0f6a3792d689f21d1a1762f55
Arkoma <aaron@adburk.net>
2018-03-02 13:10:43 -0600

    add logo for mynotater.com

--- lambda-notes/src/containers/leftRail.js
+++ lambda-notes/src/containers/leftRail.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import tater from '../images/tater.png';
+
 // import ViewNotesBtn from '../components/viewNotesBtn';
 // import CreateNewNote from '../components/createNoteBtn';
 
@@ -6,9 +8,10 @@ class LeftRail extends Component {
     render() {
         return (
             <div className="left-rail">
-                <h1 className="left-rail__title">
-                    Lambda Notes
-                </h1>
+                <div className="left-rail__title">
+                    My Note
+                    <img className="logo" src={tater} />
+                </div>
             </div>
         )   
     }
